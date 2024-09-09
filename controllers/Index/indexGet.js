10ce74032ee167cac2cee30beb499b5b20b33bc9
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2024-09-09 11:07:41 +0300

    bug fix

--- controllers/Index/indexGet.js
+++ controllers/Index/indexGet.js
@@ -1,16 +1,40 @@
 
+const School = require("../../models/School/school");
+const async = require("async");
+
 module.exports = (req, res) => {
 
-  return res.render("index/index", {
-    page: "index/index",
-    title: "Welcome",
-    includes: {
-      external: {
-        css: ["page", "general"],
-        js: ["page", "functions"]
-      }
-    }
-  })
+  const schoolsDisplayObjects = [];
+  
+  School.find({ }, (err, schools) => {
+    if (err) return res.send({ success: false, err: err });
 
+    async.timesSeries(schools.length, (i, next) => {
+      const eachSchool = schools[i];
+
+      const eachSchoolDisplayObject = {
+        schoolName: eachSchool.name,
+        schoolImage: eachSchool.profile_image
+      }
+
+      schoolsDisplayObjects.push(eachSchoolDisplayObject);
+      next();
+    }, (err) => {
+
+      if (err) return res.send({ success: false, err: err });
+
+      return res.render("index/index", {
+        page: "index/index",
+        title: "Welcome",
+        includes: {
+          external: {
+            css: ["page", "general"],
+            js: ["page", "functions"]
+          }
+        },
+        schoolsDisplayObjects
+      })
+    })
+  })
 }
 
